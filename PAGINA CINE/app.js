756cf32022eff4f5ac07e3fdf38d833b088a5018
Felixby2004 <134903953+Felixby2004@users.noreply.github.com>
2025-04-17 22:41:27 -0500

    Version 1
    Holaa :D

---
+++ PAGINA CINE/app.js
@@ -0,0 +1,75 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const modal1 = document.getElementById('modal1');
+    const modal2 = document.getElementById('modal2');
+    const modal3 = document.getElementById('modal3');
+    const modal4 = document.getElementById('modal4');
+    const modal5 = document.getElementById('modal5');
+    const modal6 = document.getElementById('modal6');
+    const modal7 = document.getElementById('modal7');
+    const modal8 = document.getElementById('modal8');
+    if (modal1) {
+        modal1.addEventListener('hidden.bs.modal', function () {
+        const iframe = modal1.querySelector('iframe');
+        if (iframe) {
+            iframe.src = iframe.src;
+        }
+    });
+    }
+    if (modal2) {
+        modal2.addEventListener('hidden.bs.modal', function () {
+        const iframe = modal2.querySelector('iframe');
+        if (iframe) {
+            iframe.src = iframe.src;
+        }
+    });
+    }
+    if (modal3) {
+        modal3.addEventListener('hidden.bs.modal', function () {
+        const iframe = modal3.querySelector('iframe');
+        if (iframe) {
+            iframe.src = iframe.src;
+        }
+    });
+    }
+    if (modal4) {
+        modal4.addEventListener('hidden.bs.modal', function () {
+        const iframe = modal4.querySelector('iframe');
+        if (iframe) {
+            iframe.src = iframe.src;
+        }
+    });
+    }
+    if (modal5) {
+        modal5.addEventListener('hidden.bs.modal', function () {
+        const iframe = modal5.querySelector('iframe');
+        if (iframe) {
+            iframe.src = iframe.src;
+        }
+    });
+    }
+    if (modal6) {
+        modal6.addEventListener('hidden.bs.modal', function () {
+        const iframe = modal6.querySelector('iframe');
+        if (iframe) {
+            iframe.src = iframe.src;
+        }
+    });
+    }
+    if (modal7) {
+        modal7.addEventListener('hidden.bs.modal', function () {
+        const iframe = modal7.querySelector('iframe');
+        if (iframe) {
+            iframe.src = iframe.src;
+        }
+    });
+    }
+    if (modal8) {
+        modal8.addEventListener('hidden.bs.modal', function () {
+        const iframe = modal8.querySelector('iframe');
+        if (iframe) {
+            iframe.src = iframe.src;
+        }
+    });
+    }
+});
